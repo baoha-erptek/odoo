4767edcf2f34a9e37829fa2f699d72052b810035
Bao Ha <bao.ha@newworldfashion.com.vn>
2026-04-30 04:50:20 +0000

    Revert "[multichannel_hub_core] feat(P1-09): add GDrive design-file upload + wizard + ACLs"
    This reverts commit 51d4aac4b7bbcd9245f8133960ba45c51c60e75b.

--- custom_addons/multichannel_hub_core/models/__init__.py
+++ custom_addons/multichannel_hub_core/models/__init__.py
@@ -3,6 +3,5 @@ from . import sale_order_fulfillment
 from . import sale_order
 from . import design_file
 from . import design_file_route
-from . import design_file_upload_wizard
 from . import sale_order_line
 from . import ir_attachment
